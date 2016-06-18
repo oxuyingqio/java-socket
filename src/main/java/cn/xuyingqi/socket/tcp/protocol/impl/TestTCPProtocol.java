9a327238a3a2ac82ebbe51719de60662a9399aca
oxuyingqio <oxuyingqio@qq.com>
2016-06-18 10:36:05 +0800

    X

--- src/main/java/cn/xuyingqi/socket/tcp/protocol/impl/TestTCPProtocol.java
+++ src/main/java/cn/xuyingqi/socket/tcp/protocol/impl/TestTCPProtocol.java
@@ -1,13 +1,44 @@
 package cn.xuyingqi.socket.tcp.protocol.impl;
 
+import java.io.IOException;
+import java.net.InetAddress;
 import java.net.Socket;
+
+import org.apache.log4j.Logger;
 
 import cn.xuyingqi.socket.tcp.protocol.TCPProtocol;
 
+/**
+ * 阻塞式TCP测试协议
+ * 
+ * @author XuYQ
+ *
+ */
 public class TestTCPProtocol implements TCPProtocol {
+
+	// 日志
+	private Logger logger = Logger.getLogger(TCPProtocol.class);
 
 	@Override
 	public void handle(Socket socket) {
 
+		// 获取客户端信息
+		InetAddress inetAddress = socket.getInetAddress();
+		int port = socket.getPort();
+		// 日志
+		this.logger.info("客户端(" + inetAddress + ":" + port + ")已连接");
+
+		try {
+			// // 关闭字节输入流
+			// this.is.close();
+			// // 关闭字节输出流
+			// this.os.close();
+			// 关闭套接字
+			socket.close();
+			// 日志
+			this.logger.info("客户端(" + inetAddress + ":" + port + ")已断开");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
--- src/test/java/cn/xuyingqi/socket/tcp/server/TCPServerTest.java
+++ src/test/java/cn/xuyingqi/socket/tcp/server/TCPServerTest.java
@@ -2,21 +2,20 @@ package cn.xuyingqi.socket.tcp.server;
 
 import java.io.IOException;
 
-import cn.xuyingqi.socket.tcp.protocol.impl.TestNioTCPProtocol;
-import cn.xuyingqi.socket.tcp.server.impl.NioTCPServer;
+import cn.xuyingqi.socket.tcp.protocol.impl.TestTCPProtocol;
 
 /**
- * 非阻塞式TCP服务测试
+ * 阻塞式TCP服务测试
  * 
  * @author XuYQ
  *
  */
-public class NioTCPServerTest {
+public class TCPServerTest {
 
 	public static void main(String[] args) {
 
 		// TCP服务器
-		TCPServer tcpServer = new NioTCPServer(new TestNioTCPProtocol(1024));
+		TCPServer tcpServer = new cn.xuyingqi.socket.tcp.server.impl.TCPServer(new TestTCPProtocol());
 		// 注册,激活服务
 		try {
 			tcpServer.init();
