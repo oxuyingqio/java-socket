6496086392120dd75d43d0701cc2b6a92b5b4b7f
oxuyingqio <oxuyingqio@qq.com>
2016-06-21 11:43:34 +0800

    X

--- src/main/java/cn/xuyingqi/socket/tcp/protocol/impl/AbstractTCPProtocol.java
+++ src/main/java/cn/xuyingqi/socket/tcp/protocol/impl/AbstractTCPProtocol.java
@@ -5,14 +5,34 @@ import java.net.Socket;
 import cn.xuyingqi.socket.tcp.protocol.TCPProtocol;
 
 /**
- * 阻塞式TCP协议
+ * 阻塞式TCP协议-责任链抽象处理者
  * 
  * @author XuYQ
  *
  */
 public abstract class AbstractTCPProtocol implements TCPProtocol {
 
+	// 后续的TCP协议处理者
+	private AbstractTCPProtocol tcpProtocol;
+
 	@Override
 	public abstract void handle(Socket socket);
 
+	/**
+	 * 获取后续的TCP协议处理者
+	 * 
+	 * @return
+	 */
+	public AbstractTCPProtocol getTcpProtocol() {
+		return tcpProtocol;
+	}
+
+	/**
+	 * 设置后续的TCP协议处理者
+	 * 
+	 * @param tcpProtocol
+	 */
+	public void setTcpProtocol(AbstractTCPProtocol tcpProtocol) {
+		this.tcpProtocol = tcpProtocol;
+	}
 }
